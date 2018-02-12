a8afffc90ca26a54ff16a420bde8ff66952ad85f
Dyego Rodrigo <dyego9444@gmail.com>
2018-02-12 00:29:50 -0200

    add math operation example

--- cbasics/src/cbasics/core.clj
+++ cbasics/src/cbasics/core.clj
@@ -7,8 +7,13 @@
   [content]
   (println content))
 
+; basic math OPSerations
+(defn sumStuff
+  [num1 num2]
+  (+ num1 num2)) ; (operator operand1 operand2 .... operandN)
+
 ; entry function
 (defn -main
   [& args]
-  (let [result (printStuff "hello clojure")]
+  (let [result (sumStuff 10 3)]
     (println result)))
